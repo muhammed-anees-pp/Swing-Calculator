60438b93c7c67379de43c1e8174cef6e0152cc1c
Muhammed Anees P P <muhammedaneespp.work@gmail.com>
2024-08-18 07:31:47 +0530

    Design Update: Key placement and size of key marking

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,18 +15,18 @@ public class Calculator implements ActionListener{
     JFrame jf;
     JLabel displayLabel;
     JButton sevenButton,eightButton,nineButton,fourButton,fiveButton,sixButton,oneButton,twoButton,threeButton;
-    JButton zeroButton,dotButton,equalButton,clearButton;
+    JButton zeroButton,dotButton,equalButton,clearButton,deleteButton,fakeButton;
     JButton divButton,multButton,subtractButton,addButton;
 
     public Calculator (){
         jf=new JFrame("Calculator");
         jf.setLayout(null);
-        jf.setSize(600,600);
-        jf.setLocation(200,200);
+        jf.setSize(450,670);
+        jf.setLocation(100,100);
 
         //Display
         displayLabel=new JLabel();
-        displayLabel.setBounds(30,50,540,40);
+        displayLabel.setBounds(30,50,380,60);
         displayLabel.setBackground(Color.gray);
         displayLabel.setOpaque(true);
         displayLabel.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -34,92 +35,117 @@ public class Calculator implements ActionListener{
 
         //Button
         sevenButton=new JButton("7");
-        sevenButton.setBounds(30,130,80,80);
+        sevenButton.setBounds(30,230,80,80);
+        sevenButton.setFont(new Font("Arial", Font.PLAIN, 40));
         sevenButton.addActionListener(this);
         jf.add(sevenButton);
 
         eightButton=new JButton("8");
-        eightButton.setBounds(130,130,80,80);
+        eightButton.setBounds(130,230,80,80);
+        eightButton.setFont(new Font("Arial", Font.PLAIN, 40));
         eightButton.addActionListener(this);
         jf.add(eightButton);
 
         nineButton=new JButton("9");
-        nineButton.setBounds(230,130,80,80);
+        nineButton.setBounds(230,230,80,80);
+        nineButton.setFont(new Font("Arial", Font.PLAIN, 40));
         nineButton.addActionListener(this);
         jf.add(nineButton);
 
         fourButton=new JButton("4");
-        fourButton.setBounds(30,230,80,80);
+        fourButton.setBounds(30,330,80,80);
+        fourButton.setFont(new Font("Arial", Font.PLAIN, 40));
         fourButton.addActionListener(this);
         jf.add(fourButton);
 
         fiveButton=new JButton("5");
-        fiveButton.setBounds(130,230,80,80);
+        fiveButton.setBounds(130,330,80,80);
+        fiveButton.setFont(new Font("Arial", Font.PLAIN, 40));
         fiveButton.addActionListener(this);
         jf.add(fiveButton);
 
         sixButton=new JButton("6");
-        sixButton.setBounds(230,230,80,80);
+        sixButton.setBounds(230,330,80,80);
+        sixButton.setFont(new Font("Arial", Font.PLAIN, 40));
         sixButton.addActionListener(this);
         jf.add(sixButton);
 
         oneButton=new JButton("1");
-        oneButton.setBounds(30,330,80,80);
+        oneButton.setBounds(30,430,80,80);
+        oneButton.setFont(new Font("Arial", Font.PLAIN, 40));
         oneButton.addActionListener(this);
         jf.add(oneButton);
 
         twoButton=new JButton("2");
-        twoButton.setBounds(130,330,80,80);
+        twoButton.setBounds(130,430,80,80);
+        twoButton.setFont(new Font("Arial", Font.PLAIN, 40));
         twoButton.addActionListener(this);
         jf.add(twoButton);
 
         threeButton=new JButton("3");
-        threeButton.setBounds(230,330,80,80);
+        threeButton.setBounds(230,430,80,80);
+        threeButton.setFont(new Font("Arial", Font.PLAIN, 40));
         threeButton.addActionListener(this);
         jf.add(threeButton);
 
-        dotButton=new JButton(".");
-        dotButton.setBounds(30,430,80,80);
-        dotButton.addActionListener(this);
-        jf.add(dotButton);
-
+        fakeButton=new JButton("");
+        fakeButton.setBounds(30,530,80,80);
+        jf.add(fakeButton);
+        
         zeroButton=new JButton("0");
-        zeroButton.setBounds(130,430,80,80);
+        zeroButton.setBounds(130,530,80,80);
+        zeroButton.setFont(new Font("Arial", Font.PLAIN, 40));
         zeroButton.addActionListener(this);
         jf.add(zeroButton);
 
-        equalButton=new JButton("=");
-        equalButton.setBounds(230,430,80,80);
-        equalButton.addActionListener(this);
-        jf.add(equalButton);
+        dotButton=new JButton(".");
+        dotButton.setBounds(230,530,80,80);
+        dotButton.setFont(new Font("Arial", Font.PLAIN, 40));
+        dotButton.addActionListener(this);
+        jf.add(dotButton);
+
+        clearButton=new JButton("C");
+        clearButton.setBounds(30,130,80,80);
+        clearButton.setFont(new Font("Arial", Font.PLAIN, 40));
+        clearButton.addActionListener(this);
+        jf.add(clearButton);
 
         divButton=new JButton("/");
-        divButton.setBounds(330,130,80,80);
+        divButton.setBounds(130,130,80,80);
+        divButton.setFont(new Font("Arial", Font.PLAIN, 40));
         divButton.addActionListener(this);
         jf.add(divButton);
 
-        multButton=new JButton("x");
-        multButton.setBounds(330,230,80,80);
+        multButton=new JButton("X");
+        multButton.setBounds(230,130,80,80);
+        multButton.setFont(new Font("Arial", Font.PLAIN, 40));
         multButton.addActionListener(this);
         jf.add(multButton);
 
+        //Have to set the functionality of these key
+        deleteButton=new JButton("D");
+        deleteButton.setBounds(330,130,80,80);
+        deleteButton.setFont(new Font("Arial", Font.PLAIN, 40));
+        deleteButton.addActionListener(this);
+        jf.add(deleteButton);
+
         subtractButton=new JButton("-");
-        subtractButton.setBounds(330,330,80,80);
+        subtractButton.setBounds(330,230,80,80);
+        subtractButton.setFont(new Font("Arial", Font.PLAIN, 40));
         subtractButton.addActionListener(this);
         jf.add(subtractButton);
 
         addButton=new JButton("+");
-        addButton.setBounds(330,430,80,80);
+        addButton.setBounds(330,330,80,80);
+        addButton.setFont(new Font("Arial", Font.PLAIN, 40));
         addButton.addActionListener(this);
         jf.add(addButton);
 
-        clearButton=new JButton("C");
-        clearButton.setBounds(430,430,80,80);
-        clearButton.addActionListener(this);
-        jf.add(clearButton);
-
-
-
+        equalButton=new JButton("=");
+        equalButton.setBounds(330,430,80,180);
+        equalButton.setFont(new Font("Arial", Font.PLAIN, 40));
+        equalButton.addActionListener(this);
+        jf.add(equalButton);
 
         jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
